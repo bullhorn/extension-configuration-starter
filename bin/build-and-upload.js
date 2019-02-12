49fa4a5a5c59ddd525cc8873d8754ee265d2a412
John Sullivan <john.sullivan@bullhorn.com>
2019-02-12 13:21:37 -0500

    Fixed issues with multi-user config.

--- bin/build-and-upload.js
+++ bin/build-and-upload.js
@@ -19,13 +19,30 @@ if(!fs.existsSync(fileName)) {
 
 const configuration = JSON.parse(fs.readFileSync(fileName, 'UTF-8'));
 
-if(!configuration || ((!configuration.username || !configuration.password) && !configuration.users)) {
-  console.log(`Configuration loaded does not have a username or password`, configuration);
+function hasUsernameAndPassword(configuration) {
+  return configuration.username && configuration.password;
+}
+
+function hasUsers(configuration) {
+  if(Array.isArray(configuration.users) && configuration.users.length > 0) {
+    const invalidUsers = configuration.users.filter(user => {
+      return !user.username || !user.password || !user.privateLabelId;
+    });
+
+    return invalidUsers.length === 0;
+  }
+
+  return false;
+}
+
+if(!configuration || (!hasUsernameAndPassword(configuration) && !hasUsers(configuration))) {
+  console.log(`Configuration should have either a username or password, or an array of users that each have a username, password, and privateLabelId`, configuration);
 
   process.exit();
 }
 
 const cmdSuffix = /^win/.test(process.platform) ? '.cmd' : '';
+const extensionsFileName = `./output/extension.json`;
 const lineBreaks = /(?:\r\n|\r|\n)/g;
 
 function print(command, arguments, callback) {
@@ -48,6 +65,18 @@ function print(command, arguments, callback) {
       callback();
     }
   });
+
+  return process;
+}
+
+function getExtensionFile() {
+  if(!fs.existsSync(extensionsFileName)) {
+    console.error('Error performing extract command; cannot remove page interactions');
+
+    process.exit(1);
+  }
+
+  return fs.readFileSync(extensionsFileName, 'utf-8');
 }
 
 function clean(callback) {
@@ -71,15 +100,8 @@ function injectScript(configuration, script) {
 }
 
 function inject(configuration, callback) {
-  const file = `./output/extension.json`;
-
-  if(!fs.existsSync(file)) {
-    console.error('Error performing extract command; cannot inject environment variables');
-
-    process.exit();
-  }
-
-  const extensions = JSON.parse(fs.readFileSync(file, 'utf-8'));
+  const file = getExtensionFile();
+  const extensions = JSON.parse(file);
 
   if(extensions.fieldInteractions) {
     Object.keys(extensions.fieldInteractions).forEach( entity => {
@@ -97,7 +119,7 @@ function inject(configuration, callback) {
     }
   }
 
-  jsonfile.writeFileSync(file, extensions, {
+  jsonfile.writeFileSync(extensionsFileName, extensions, {
     spaces: 2,
   });
 
@@ -107,11 +129,72 @@ function inject(configuration, callback) {
 }
 
 function auth(username, password, callback) {
-  print(`bullhorn${cmdSuffix}`,  [ 'auth', 'login', `--username=${username}`, `--password=${password}` ], callback);
+  const auth = print(`bullhorn${cmdSuffix}`,  [ 'auth', 'login', `--username=${username}`, `--password=${password}` ], callback);
+
+  auth.stdout.on('data', (data) => {
+    const content = data.toString().replace(lineBreaks, '');
+
+    if(content.toLowerCase().indexOf('error') !== -1) {
+      console.log('Error occurred during authorization, exiting.');
+
+      process.exit(1);
+    }
+  });
 }
 
 function upload(callback) {
   print(`bullhorn${cmdSuffix}`,  [ 'extensions', 'upload', '--force=true', '--skip=true' ], callback);
+}
+
+function authAndUpload(username, password, callback) {
+  auth(username, password, () => {
+    upload(callback)
+  });
+}
+
+function getExtensionId() {
+  const file = getExtensionFile();
+  const extensions = JSON.parse(file);
+
+  return extensions.name;
+}
+
+function updatePrivateLabelId(extensionId, privateLabelId) {
+  const file = getExtensionFile();
+  const extensions = JSON.parse(file);
+
+  let newExtensionId = `${privateLabelId}-interactions`;
+
+  if(extensionId.indexOf('-interactions') > 0) {
+    const name = extensionId.substring(0, extensionId.indexOf('-interactions'));
+
+    newExtensionId = `${name}-${privateLabelId}-interactions`;
+  }
+
+  extensions.name = newExtensionId;
+
+  console.log(`Updating extensions name to ${newExtensionId} before auth and upload`);
+
+  jsonfile.writeFileSync(extensionsFileName, extensions, {
+    spaces: 2,
+  });
+}
+
+function stripPageInteractions(callback) {
+  const file = getExtensionFile();
+  const extensions = JSON.parse(file);
+
+  if(extensions.pageInteractions) {
+    extensions.pageInteractions = [];
+  }
+
+  jsonfile.writeFileSync(extensionsFileName, extensions, {
+    spaces: 2,
+  });
+
+  console.log('Successfully removed page interactions for additional deploy');
+
+  callback();
 }
 
 try {
@@ -120,18 +203,30 @@ try {
       extract(() => {
         inject(configuration, () => {
           if(configuration.username && configuration.password) {
-            auth(configuration.username, configuration.password, () => {
-              upload(() => {
-                console.log('Successfully uploaded!');
-              })
+            authAndUpload(configuration.username, configuration.password, () => {
+              console.log('Successfully uploaded!');
             });
           } else if(configuration.users && Array.isArray(configuration.users) && configuration.users.length > 0) {
-            configuration.users.forEach(user => {
-              auth(user.username, user.password, () => {
-                upload(() => {
-                  console.log('Successfully uploaded!');
-                })
-              });
+            const extensionId = getExtensionId();
+
+            updatePrivateLabelId(extensionId, configuration.users[0].privateLabelId);
+
+            authAndUpload(configuration.users[0].username, configuration.users[0].password, () => {
+              console.log('Successfully uploaded all interactions for user ' + configuration.users[0].username);
+
+              const additionalUsers = configuration.users.slice(1);
+
+              if(additionalUsers && additionalUsers.length > 0) {
+                stripPageInteractions(() => {
+                  additionalUsers.forEach(user => {
+                    updatePrivateLabelId(extensionId, user.privateLabelId);
+
+                    authAndUpload(user.username, user.password, () => {
+                      console.log('Successfully uploaded field interactions for user ' + user.username);
+                    });
+                  });
+                });
+              }
             });
           }
         });
